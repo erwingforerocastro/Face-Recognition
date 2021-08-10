1c457e0fa1590922d40368619194161cc23fd062
Erwing Forero Castro <erwingforerocastro@gmail.com>
2021-08-09 22:05:36 -0500

    modify library

--- JavaScript/src/mvfy/utils.js
+++ JavaScript/src/mvfy/utils.js
@@ -1,20 +1,6 @@
 const moment = require('moment');
 moment.locale('es');
 
-const convertString2Int = (string) => {
-    let sub_string = string.match(/\d+/gi)
-    return (sub_string) ? parseInt(sub_string.join("")) : 0
-}
-
-const getActualDate = (format = 'day') => {
-
-}
-
-const extractValidDate = (_date) => {
-    const formats = [
-        ``
-    ]
-}
 
 module.exports = {
     convertString2Int
